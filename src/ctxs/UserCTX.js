805aeaa715ba2e00f4692aa2fbd3a4a4ca08f921
akiraei <akiraeiphoto@gmail.com>
2018-07-10 18:08:48 +0900

    init

---
+++ src/ctxs/UserCTX.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+
+const { Provider, Consumer } = React.createContext();
+
+class UserProvider extends Component {
+  state = {
+    activities: [
+      // {
+      //   id: 1,
+      //   taskId: 1,
+      //   userId: 1,
+      //   body: '완료된 작업 별 정렬 구현중',
+      //   logDate: '2018.06.01 2:41:48',
+      // },
+    ]
+  };
+
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+    try {
+      const me = await serverAPI.get('/me');
+      const res = await serverAPI.get('/activities');
+      this.setState({
+        me: me.data.id,
+        activities: res.data,
+        loading: false,
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  Create = async o => {
+    this.setState({ loading: true });
+    try {
+      const res = await serverAPI.post('/activities', o);
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: res.data.id,
+      });
+    } catch (e) {
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: '',
+      });
+    }
+  };
+
+  Delete = async id => {
+    this.setState({ loading: true });
+    try {
+      await serverAPI.delete(`/activities/${id}`);
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: '',
+      });
+    } catch (e) {
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: '',
+      });
+    }
+  };
+
+  Update = async (id, keyType, body) => {
+    this.setState({ loading: true });
+    try {
+      await serverAPI.patch(`/activities/${id}`, {
+        [keyType]: body,
+      });
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: '',
+      });
+    } catch (e) {
+      const get = await serverAPI.get('/activities');
+      this.setState({
+        activities: get.data,
+        loading: false,
+        target: '',
+      });
+    }
+  };
+
+  render() {
+    const value = {
+      activityState: this.state,
+      activityFunc: {
+        Create: this.Create,
+        Delete: this.Delete,
+        Update: this.Update,
+      },
+    };
+    return <Provider value={value}>{this.props.children}</Provider>;
+  }
+}
+
+export { UserProvider, Consumer as UserConsumer };
